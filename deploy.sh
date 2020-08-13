632ffd6aa9be9c04d42aed73af7e3afc81efb748
sellto <selleslagh.tom@gmail.com>
2020-08-13 15:10:16 +0200

    initial Commit

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#Install Helm
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl taint nodes --all node-role.kubernetes.io/master-
+#CNI
+kubectl apply -f ./flannel
+
+
+#LoadBalancer
+#see what changes would be made, returns nonzero returncode if different
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl diff -f - -n kube-system
+
+# actually apply the changes, returns nonzero returncode on errors only
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl apply -f - -n kube-system
+
+kubectl apply -f ./namespaces/MetalLB.yaml
+kubectl apply  --recursive -f ./metallb
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+# VolumeProvisioner
+kubectl apply -f ./namespaces/Longhorn.yaml
+kubectl apply -f --recursive ./longhorn
+kubectl patch storageclass longhorn -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+
+#IngressController
+kubectl apply -f ./traefik
